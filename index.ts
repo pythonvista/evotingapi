bc5efb173314f1a10ef4d385e7a400c2ec10b817
pythonvista <emeterv18@gmail.com>
2023-08-14 15:28:35 +0100

    dsds

--- index.ts
+++ index.ts
@@ -48,7 +48,7 @@ const MemoryStore = memoryStore(session);
 const {
   ENABLE_CONFORMANCE,
   ENABLE_HTTPS,
-  RP_ID = 'https://evotingclient.vercel.app',
+  RP_ID = 'https://evotingclient.vercel.app/fingerscanner',
 } = process.env;
 
 app.use(express.static('./public/'));
