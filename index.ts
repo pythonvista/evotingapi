f4834c6208ac410c7102196af3b5f8b27fdda55e
pythonvista <nablintech@gmail.com>
2025-01-09 16:05:19 -0800

    agh

--- index.ts
+++ index.ts
@@ -305,7 +305,7 @@ app.post('/verify-authentication', async (req, res) => {
 });
 
 if (ENABLE_HTTPS) {
-  const host = 'localhost';
+  const host = '0.0.0.0';
   const port = 443;
   expectedOrigin = `https://evotingapi.onrender.com`;
 
@@ -325,7 +325,7 @@ if (ENABLE_HTTPS) {
     });
 } else {
  const host = 'localhost';
-  const port = 10000;
+  const port = 8000;
   expectedOrigin = `https://evotingapi.onrender.com`;
 
   http.createServer(app).listen(port, host, () => {
